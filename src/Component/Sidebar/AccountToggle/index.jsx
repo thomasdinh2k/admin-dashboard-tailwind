a7f4a68b8da8c2aef46eec422cd1e740d5077bac
thomasdinh <work.dnstung@gmail.com>
2024-09-16 14:17:30 +0700

    complete account toggle

--- src/Component/Sidebar/AccountToggle/index.jsx
+++ src/Component/Sidebar/AccountToggle/index.jsx
@@ -1,14 +1,27 @@
-export const AccountToggle = () => {
-  return (
-    <div>
-        <button>
-            <img src="https://api.dicebear.com/9.x/lorelei/svg" alt="" />
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
 
-            <div>
-                <span>Thomas Dinh</span>
-                <span>work.dnstung@gmail.com</span>
-            </div>
-        </button>
-    </div>
-  )
-}
+export const AccountToggle = () => {
+	return (
+		<div className="border-b mb-4 mt-2 pb-4 border-stone-300">
+			<button className="flex p-0.5 hover:bg-stone-200 rounded transition-colors relative gap-2 w-full items-center">
+				<img
+					className="size-8 rounded shrink-0 bg-gray-600 shadow"
+					src="https://api.dicebear.com/9.x/lorelei/svg"
+					alt="avatar"
+				/>
+
+				<div className="text-start">
+					<span className="text-sm font-bold block text-nowrap overflow-hidden">
+						Dinh Ng. Son Tung
+					</span>
+					<span className="text-xs block text-stone-500">
+						Thomas Dinh
+					</span>
+				</div>
+
+				<FiChevronDown className="absolute right-2 top-1/2 translate-y-[calc(-50%+4px)] text-xs" />
+				<FiChevronUp className="absolute right-2 top-1/2 text-xs  translate-y-[calc(-50%-4px)]" />
+			</button>
+		</div>
+	);
+};
