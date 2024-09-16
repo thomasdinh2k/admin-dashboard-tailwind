aa658e78320671f16578952cfea01c585a864090
thomasdinh <work.dnstung@gmail.com>
2024-09-16 11:06:57 +0700

    sizing pannels

---
+++ src/Component/Sidebar/AccountToggle/index.jsx
@@ -0,0 +1,14 @@
+export const AccountToggle = () => {
+  return (
+    <div>
+        <button>
+            <img src="https://api.dicebear.com/9.x/lorelei/svg" alt="" />
+
+            <div>
+                <span>Thomas Dinh</span>
+                <span>work.dnstung@gmail.com</span>
+            </div>
+        </button>
+    </div>
+  )
+}
--- src/Component/Sidebar/index.jsx
+++ src/Component/Sidebar/index.jsx
@@ -1,10 +1,22 @@
+import { AccountToggle } from "./AccountToggle";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+
 export default function Sidebar() {
+	/* Height of the panel will be:
+    panelHeight = screen full height - top plan toggle - global top/bottom padding
+                = 100vh - 12rem - (4rem * 2) 
+    */
+	const panelHeight = `h-[calc(100vh-48px-32px)]`;
+
 	return (
 		<div>
-			<div className="overflow-y-scroll bg-teal-300">
-                Main Content
-                {/* [ ] Main sidebar content */} 
-            </div>
+			<div
+				className={
+					"overflow-y-scroll sticky top-4 " + panelHeight
+				}
+			>
+				<AccountToggle />
+			</div>
 
 			{/* [ ] Plan toggle */}
 		</div>
