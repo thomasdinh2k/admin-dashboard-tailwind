ada680391656c4835dd52db671252dcd018099cf
thomasdinh <work.dnstung@gmail.com>
2024-09-18 11:27:08 +0700

    complete project

--- src/Component/Dashboard/Grid/index.jsx
+++ src/Component/Dashboard/Grid/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import StatsCards from '../StatCards';
 import ActivityGraph from '../ActivityGraph';
+import UsageRadar from '../UsageRadar';
+import RecentTransactions from '../RecentTransactions';
 
 const Grid = () => {
   return (
     <div className='px-4 grid gap-3 grid-cols-12'>
         <StatsCards />
 
-        <ActivityGraph />        
+        <ActivityGraph />
+        <UsageRadar />        
+        <RecentTransactions />
     </div>
   )
 }
